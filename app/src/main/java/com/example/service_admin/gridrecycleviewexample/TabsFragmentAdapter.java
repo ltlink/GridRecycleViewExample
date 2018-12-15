6a610f5b6ba53dcd35bbee2069a3856eff1e14ca
Eduardo Zavala <eduramzav@hotmail.com>
2018-12-15 12:04:15 -0600

    initial commit

---
+++ app/src/main/java/com/example/service_admin/gridrecycleviewexample/TabsFragmentAdapter.java
@@ -0,0 +1,21 @@
+package com.example.service_admin.gridrecycleviewexample;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+public class TabsFragmentAdapter extends FragmentPagerAdapter {
+    public TabsFragmentAdapter(FragmentManager fm) {
+        super(fm);
+    }
+
+    @Override
+    public Fragment getItem(int i) {
+        return null;
+    }
+
+    @Override
+    public int getCount() {
+        return 0;
+    }
+}
